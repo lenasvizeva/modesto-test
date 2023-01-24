150b1a61e49bb3c87a8feec18e1f3379fea12f73
elena <kaiout@mail.ru>
2023-01-24 16:44:36 +0400

    bugs fixes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,13 @@
+// event handlers functions 
+
 const sections = document.getElementsByClassName("feedback-form__section")
 
 function regFormOnClickForward() {
-    sections[0].style.display = 'none'
-    sections[1].style.display = 'flex'
+    sections[0].classList.remove('active')
+    sections[1].classList.add('active')
 }
 
 function regFormOnClickBack() {
-    sections[1].style.display = 'none'
-    sections[0].style.display = 'flex'
+    sections[1].classList.remove('active')
+    sections[0].classList.add('active')
 }
