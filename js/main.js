17d87a9ed0e5710b55c942e9071ac8080638c864
elena <kaiout@mail.ru>
2023-01-25 17:03:12 +0400

    Updates

---
+++ js/main.js
@@ -0,0 +1,1 @@
+"use strict";var sections=document.getElementsByClassName("feedback-form__section");function regFormOnClickForward(){sections[0].classList.remove("active"),sections[1].classList.add("active")}function regFormOnClickBack(){sections[1].classList.remove("active"),sections[0].classList.add("active")}
